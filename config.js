0886cab3d1db3c14865a9e91a1ff127c385e73ca
dg-lk <denuwaralakshan634@gmail.com>
2026-04-14 15:12:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD=98W2kpwRd3bM7FT',
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?1hOrkl',
     PORT: process.env.PORT || 8000,
     SESSION_NAME: process.env.SESSION_NAME || "auth_info_baileys"
 };
